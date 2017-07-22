cc3e0bc157292f7db59c45d8dbbb8cd2688c9735
Exmortem <dconnelly@live.com>
2017-07-22 12:32:59 -0600

    added GetById endpoint

--- MagitekApi/Controllers/MagitekSettingsController.cs
+++ MagitekApi/Controllers/MagitekSettingsController.cs
@@ -4,12 +4,15 @@ using System.Threading.Tasks;
 using MagitekApi.Database;
 using MagitekApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagitekApi.Controllers
 {
     [Route("/api/[controller]")]
     public class MagitekSettingsController : Controller
     {
+        #region GET
+
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -23,6 +26,29 @@ namespace MagitekApi.Controllers
             return new OkObjectResult(magitekSettingsList);
         }
 
+        [HttpGet]
+        [Route("/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            MagitekSettings settings;
+
+            using (var context = MagitekContextFactory.Create())
+            {
+                settings = await context.MagitekSettings.FirstOrDefaultAsync(r => r.Id == id);
+            }
+
+            if (settings == null)
+            {
+                return new BadRequestResult();
+            }
+
+            return new OkObjectResult(settings);
+        }
+
+        #endregion  
+
+        #region POST
+
         [HttpPost("add")]
         public async Task<IActionResult> Add([FromBody] MagitekSettings settings)
         {
@@ -34,5 +60,7 @@ namespace MagitekApi.Controllers
 
             return Ok();
         }
+
+        #endregion
     }
 }
