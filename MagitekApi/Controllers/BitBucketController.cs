4a00ecab56278684137a3b2ebf540199f043830b
Exmortem <dconnelly@live.com>
2018-03-03 14:30:37 -0700

    Added BitBucket controller

---
+++ MagitekApi/Controllers/BitBucketController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using MagitekApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace MagitekApi.Controllers
+{
+    [Route("[controller]")]
+    public class BitBucketController : Controller
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BitBucketController(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBosses([FromBody] string message)
+        {
+            Console.WriteLine(message);
+            return new OkResult();
+        }
+    }
+}
---
+++ MagitekApi/Controllers/BossesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using MagitekApi.Database;
+using MagitekApi.Extensions;
+using MagitekApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace MagitekApi.Controllers
+{
+    [Route("[controller]")]
+    public class BossesController : Controller
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BossesController(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        [HttpGet("verify/{secretKey}")]
+        public async Task<IActionResult> GetAllBosses(string secretKey)
+        {
+            var contributor = _redisCache.Get(secretKey).FromByteArray<Contributor>();
+
+            if (contributor != null)
+            {
+                Console.WriteLine($"Got {contributor.Name} from Redis Cache");
+                return new ObjectResult(contributor);
+            }
+
+            using (var context = MagitekContextFactory.Create())
+            {
+                contributor = await context.Contributors.FirstOrDefaultAsync(r => r.SecretKey == secretKey);
+            }
+
+            return contributor == null ?
+                new BadRequestObjectResult(new MagitekApiResult() { Name = "Failure", Description = $"Key Is Not Valid" }) :
+                new ObjectResult(contributor);
+        }
+    }
+}
